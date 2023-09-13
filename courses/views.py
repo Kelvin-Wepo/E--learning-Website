8c9eda5fa15cda1668553e44ed012e2de29f6b0b
kelvin wepo <kelvinwepo7710@gmail.com>
2023-09-13 19:59:49 +0300

    refactored code for better readability

--- courses/views.py
+++ courses/views.py
@@ -110,3 +110,10 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CourseDetailView, self).get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(initial={'course': self.object})
+        context['review_form'] = ReviewForm()
+        context['reviews'] = Review.objects.order_by('-pub_date')[:9]
+        return context
