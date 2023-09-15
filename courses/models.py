e02927b65626afa1407782a288ca51d7248eb0d4
kelvin wepo <kelvinwepo7710@gmail.com>
2023-09-15 15:54:16 +0300

    feat:this commit introduces subject and relevant field

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models import Avg
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from courses.fields import OrderField
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+from django.utils.text import slugify
+from django.utils import timezone
+from autoslug import AutoSlugField
 
-# Create your models here.
+import numpy as np
+
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
