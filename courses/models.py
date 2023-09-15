f8081ffa367df5565453816359ef23e847e57f7e
kelvin wepo <kelvinwepo7710@gmail.com>
2023-09-15 16:00:23 +0300

    feat:(models)this commit introduces course models with relevant field and relationships

--- courses/models.py
+++ courses/models.py
@@ -22,3 +22,20 @@ class Subject(models.Model):
     def __str__(self):
         return self.title
 
+class Course(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='courses_created', on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, related_name='courses', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = AutoSlugField(populate_from='title', unique_with='created__month')
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    students = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='courses_joined', blank=True)
+
+    class Meta:
+        ordering = ('-created',)
+    def average_rating(self):
+        return self.reviews.aggregate(Avg('rating'))['rating__avg']
+
+    def __str__(self):
+        return self.title
+
