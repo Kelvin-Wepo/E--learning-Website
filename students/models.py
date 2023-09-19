8a9972947555bca2bdeb83a1a9467136382a3b53
kelvin wepo <kelvinwepo7710@gmail.com>
2023-09-19 19:38:16 +0300

    feat:this commit implements a tag model

--- students/models.py
+++ students/models.py
@@ -11,3 +11,17 @@ class User(AbstractUser):
     is_teacher = models.BooleanField(default=False)
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+    color = models.CharField(max_length=7, default='#007bff')
+
+    def __str__(self):
+        return self.name
+
+    def get_html_badge(self):
+        name = escape(self.name)
+        color = escape(self.color)
+        html = '<span class="badge badge-primary" style="background-color: %s">%s</span>' % (color, name)
+        return mark_safe(html)
+
+
