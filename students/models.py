65b616b43a18d28ff112cb95e6e4571737394f48
kelvin wepo <kelvinwepo7710@gmail.com>
2023-09-24 21:03:02 +0300

    feat: this commit adds user type flags

--- students/models.py
+++ students/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.signals import post_save
+from django.utils.html import escape, mark_safe
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
+
