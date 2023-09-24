0509d87c9fc8ee3cb87e80ea01c2e5cd96f395c6
kelvin wepo <kelvinwepo7710@gmail.com>
2023-09-24 21:23:01 +0300

    feat:this commit creates implements taken quiz model and students answers

--- students/models.py
+++ students/models.py
@@ -93,6 +93,26 @@ class TakenQuiz(models.Model):
     def __str__(self):
         return '{}. {} {}%'.format(self.quiz, self.student, self.score)
 
+class TakenQuiz(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='taken_quizzes')
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='taken_quizzes')
+    score = models.FloatField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{}. {} {}%'.format(self.quiz, self.student, self.score)
+
+
+class StudentAnswer(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='quiz_answers')
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name='+')
+
+    def __str__(self):
+        return '{} {}'.format(self.student, self.answer)
+
+
+
+
 
 
 
